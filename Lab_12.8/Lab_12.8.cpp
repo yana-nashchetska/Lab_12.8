1b040307da404794e7b8a909072e2fd9ebac8d7d
Yana Nashchetska <yana.nashchetska.ri.2022@lpnu.ua>
2023-05-25 22:59:40 +0300

    commit first

---
+++ Lab_12.8/Lab_12.8.cpp
@@ -0,0 +1,124 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <fstream>
+
+using namespace std;
+
+struct Textbook
+{
+    string author;
+    string title;
+    int positiveReviews;
+    Textbook* prev;
+    Textbook* next;
+};
+
+typedef Textbook* TextbookPtr;
+
+void Insert(TextbookPtr& head, TextbookPtr& tail, const string& author, const string& title, int positiveReviews);
+void PrintList(TextbookPtr head);
+void FilterList(TextbookPtr& head, TextbookPtr& tail, int minPositiveReviews);
+
+int main()
+{
+    TextbookPtr head = nullptr;
+    TextbookPtr tail = nullptr;
+
+    ifstream inputFile("D:\\АП\\Лабораторні роботи\\Lab_12\\Lab_12.8\\books.txt");
+    if (inputFile.is_open())
+    {
+        string author, title;
+        int positiveReviews;
+
+        while (inputFile >> author >> title >> positiveReviews)
+        {
+            Insert(head, tail, author, title, positiveReviews);
+        }
+
+        inputFile.close();
+
+        cout << " Input List:" << endl;
+        PrintList(head);
+
+        int minReviews;
+        cout << "\n Enter the minimum number of positive reviews: ";
+        cin >> minReviews;
+
+        FilterList(head, tail, minReviews);
+
+        cout << "\n Output List:" << endl;
+        PrintList(head);
+    }
+    else
+    {
+        cout << " Failed to open the input file." << endl;
+    }
+
+    return 0;
+}
+
+void Insert(TextbookPtr& head, TextbookPtr& tail, const string& author, const string& title, int positiveReviews)
+{
+    TextbookPtr newNode = new Textbook;
+    newNode->author = author;
+    newNode->title = title;
+    newNode->positiveReviews = positiveReviews;
+    newNode->prev = nullptr;
+    newNode->next = nullptr;
+
+    if (head == nullptr)
+    {
+        head = newNode;
+        tail = newNode;
+    }
+    else
+    {
+        tail->next = newNode;
+        newNode->prev = tail;
+        tail = newNode;
+    }
+}
+
+void PrintList(TextbookPtr head)
+{
+    TextbookPtr current = head;
+
+    while (current != nullptr)
+    {
+        cout << " Author: " << current->author << ", Title: " << current->title
+            << ", Positive Reviews: " << current->positiveReviews << endl;
+
+        current = current->next;
+    }
+}
+
+void FilterList(TextbookPtr& head, TextbookPtr& tail, int minPositiveReviews)
+{
+    if (head == nullptr)
+    {
+        return;
+    }
+
+    if (head->positiveReviews < minPositiveReviews)
+    {
+        TextbookPtr temp = head;
+        head = head->next;
+
+        if (head != nullptr)
+        {
+            head->prev = nullptr;
+        }
+        else
+        {
+            tail = nullptr;
+        }
+
+        delete temp;
+
+        FilterList(head, tail, minPositiveReviews);
+    }
+    else
+    {
+        FilterList(head->next, tail, minPositiveReviews);
+    }
+}
---
+++ Lab_12.8_test/Lab_12.8_test.cpp
@@ -0,0 +1,33 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab_12.8/Lab_12.8.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Lab128test
+{
+	TEST_CLASS(Lab128test)
+	{
+	public:
+
+		TEST_METHOD(TestInsert)
+		{
+			TextbookPtr head = nullptr;
+			TextbookPtr tail = nullptr;
+
+			// Додати елементи до списку
+			Insert(head, tail, "Author1", "Title1", 10);
+		
+			string expected_author1 = "Author1";
+			string expected_title1 = "Title1";
+			int expected_reviews1 = 10;
+
+			// Перевіряєо чи правильно додались елементи
+			Assert::AreEqual(expected_author1, head->author);
+			Assert::AreEqual(expected_title1, head->title);
+			Assert::AreEqual(expected_reviews1, head->positiveReviews);
+
+
+		}
+	};
+}
